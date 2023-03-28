5d3274aa461a61f632a47a158815c489254e356d
YevhenHryhorenko <116802765+YevhenHryhorenko@users.noreply.github.com>
2023-03-28 23:49:32 +0300

    +

--- js/main.js
+++ js/main.js
@@ -53,16 +53,13 @@ document.write(`<br><br>`)
 
 document.write(`Fifth task <br>`)
 
-// res = 1
-// let flag
+res = 15
 
-// for (let index = 15; index < 36; index++) {
-//     flag = index * (index + 1)
-//     console.log(`${res}\n`);
-//     res = index * flag
-// }
+for (let index = 16; index < 36; index++) {
+  res = res * index
+}
 
-// document.write(`${res}`)
+document.write(`${res}`)
 
 document.write(`<br><br>`)
 
@@ -100,3 +97,58 @@ for (let index = 100; index < 201; index++) {
   }
 }
 
+document.write(`<br><br>`)
+
+document.write(`Nineth task <br>`)
+
+const num = +prompt("Enter number from N", 20)
+
+for (let index = 1; index <= num; index++) {
+  if (num % index == 0) {
+    document.write(`${index}`)
+    index != num ? document.write(`${comma} `) : document.write(`${dot}`)
+  }
+}
+
+document.write(`<br><br>`)
+
+document.write(`Tenth task <br>`)
+let counter = 0
+
+for (let index = 1; index <= num; index++) {
+  if (num % index == 0) {
+    if (index % 2 == 0) {
+      counter++
+
+    }
+  }
+}
+
+document.write(`${counter}`)
+
+document.write(`<br><br>`)
+
+document.write(`Eleventh task <br>`)
+res = 0
+
+for (let index = 1; index <= num; index++) {
+  if (num % index == 0) {
+    if (index % 2 == 0) {
+      res += index
+    }
+  }
+}
+
+document.write(`${res}`)
+
+document.write(`<br><br>`)
+
+document.write(`Twelfth task <br>`)
+
+document.write(`<table style="border: 2px solid blue; background-color: yellow">`)
+for (let index = 1; index < 10; index++) {
+  for (let j = 1; j < 10; j++) {
+    document.write(`<tr> <td style="border: 2px solid blue; padding: 5px"> ${index} * ${j}</td> <td style="border: 2px solid blue; padding: 5px">${index * j}</td> </tr>`)
+  }
+}
+document.write(`</table>`)
